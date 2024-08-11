fad25bfcc13140b227fa97c57b631a5435d6c38a
Melis Olmez <olmezmelis204@gmail.com>
2024-08-11 16:33:11 +0300

    add project

---
+++ src/main/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplication.java
@@ -0,0 +1,13 @@
+package dev.melis.EngelsizGonuller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngelsizGonullerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngelsizGonullerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.melis.EngelsizGonuller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngelsizGonullerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
