4df9438e23012152350d58f3f8a5a2a0d324744d
Melis Olmez <olmezmelis204@gmail.com>
2024-09-09 22:42:50 +0300

    add user repository

---
+++ src/main/java/dev/melis/EngelsizGonuller/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package dev.melis.EngelsizGonuller.repository;
+
+import dev.melis.EngelsizGonuller.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+      Optional<User> findByEmail(String email);
+
+}
