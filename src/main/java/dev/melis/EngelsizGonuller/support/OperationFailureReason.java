a06b0fb40edcbde97cb013568bac91442a127e66
Melis Olmez <olmezmelis204@gmail.com>
2024-09-09 22:47:37 +0300

    add creation result

---
+++ src/main/java/dev/melis/EngelsizGonuller/support/OperationFailureReason.java
@@ -0,0 +1,9 @@
+package dev.melis.EngelsizGonuller.support;
+
+public enum OperationFailureReason {
+    NOT_FOUND,
+    CONFLICT,
+    UNAUTHORIZED,
+    ALREADY_REPORTED,
+    PRECONDITION_FAILED
+}
