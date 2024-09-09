f5213681375777aea31cd2256a83c96e0d388ebd
Melis Olmez <olmezmelis204@gmail.com>
2024-09-09 22:40:29 +0300

    add registration controller

---
+++ src/main/java/dev/melis/EngelsizGonuller/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package dev.melis.EngelsizGonuller.controller;
+
+import dev.melis.EngelsizGonuller.service.registration.RegistrationService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping("/auth/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationControllerRequest request){
+        var result= registrationService.register(request.toServiceRequest());
+        if(!result.isSuccess()){
+            return new ResponseEntity<>("User has already registered",HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/confirm-account", method= {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<?> confirmUserAccount(@RequestParam("token")String confirmationToken) {
+        return registrationService.confirmEmail(confirmationToken);
+    }
+}
