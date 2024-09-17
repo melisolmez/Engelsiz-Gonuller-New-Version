a8d1491d9ccf5bf53b213c37a57994bc613b8390
Melis Olmez <olmezmelis204@gmail.com>
2024-09-17 22:28:58 +0300

    update

--- src/main/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplication.java
+++ src/main/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplication.java
@@ -1,5 +1,6 @@
 package dev.melis.EngelsizGonuller;
 
+import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class EngelsizGonullerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EngelsizGonullerApplication.class, args);
-	}
 
+
+	}
 }
