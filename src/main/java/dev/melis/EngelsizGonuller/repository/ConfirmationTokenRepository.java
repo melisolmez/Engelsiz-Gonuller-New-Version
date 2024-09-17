9536beb804f5710580cb271f1354cc952c7530ac
Melis Olmez <olmezmelis204@gmail.com>
2024-09-17 22:25:42 +0300

    add confirm token repository

--- src/main/java/dev/melis/EngelsizGonuller/repository/ConfirmationTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package dev.melis.EngelsizGonuller.repository;
-
-import dev.melis.EngelsizGonuller.model.ConfirmationToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken,Long> {
-    ConfirmationToken findByConfirmationToken(String confirmationToken);
-}
