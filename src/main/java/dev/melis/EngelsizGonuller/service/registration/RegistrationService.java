13d4f952a7d635774bd18c97fd40769e942e35f9
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:53:52 +0300

    update RegistrationService

--- src/main/java/dev/melis/EngelsizGonuller/service/registration/RegistrationService.java
+++ src/main/java/dev/melis/EngelsizGonuller/service/registration/RegistrationService.java
@@ -1,10 +1,12 @@
 package dev.melis.EngelsizGonuller.service.registration;
 
+import dev.melis.EngelsizGonuller.model.User;
 import dev.melis.EngelsizGonuller.support.CreationResult;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 
 public interface RegistrationService {
+
   CreationResult register(@Valid RegistrationServiceRequest request);
 
 }
