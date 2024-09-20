ed5b76901caf204f83f9425cc38bc477b39722a0
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:53:17 +0300

    update registration service

--- src/main/java/dev/melis/EngelsizGonuller/service/registration/DefaultRegistrationService.java
+++ src/main/java/dev/melis/EngelsizGonuller/service/registration/DefaultRegistrationService.java
@@ -2,10 +2,18 @@ package dev.melis.EngelsizGonuller.service.registration;
 
 import dev.melis.EngelsizGonuller.model.User;
 import dev.melis.EngelsizGonuller.model.UserType;
+import dev.melis.EngelsizGonuller.model.VerificationToken;
 import dev.melis.EngelsizGonuller.repository.UserRepository;
+import dev.melis.EngelsizGonuller.repository.VerificationTokenRepository;
+import dev.melis.EngelsizGonuller.service.email.EmailService;
+import dev.melis.EngelsizGonuller.service.email.VerificationTokenService;
 import dev.melis.EngelsizGonuller.support.CreationResult;
 import dev.melis.EngelsizGonuller.support.OperationFailureReason;
 import dev.melis.EngelsizGonuller.support.emialvalidator.EmailValidator;
+import dev.melis.EngelsizGonuller.support.passwordencoder.PasswordEncoderAdaptor;
+import java.util.Calendar;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -15,13 +23,19 @@ import java.util.Optional;
 public class DefaultRegistrationService implements RegistrationService{
 
     private final UserRepository repository;
+    private final VerificationTokenService tokenService;
+    private final EmailService emailService;
+    private final VerificationTokenRepository verificationTokenRepository;
 
 
-    public DefaultRegistrationService(UserRepository repository) {
+    public DefaultRegistrationService(UserRepository repository, VerificationTokenService tokenService, EmailService emailService,
+        VerificationTokenRepository verificationTokenRepository) {
         this.repository = repository;
+        this.tokenService = tokenService;
+        this.emailService = emailService;
+        this.verificationTokenRepository = verificationTokenRepository;
     }
 
-    // security eklendiğinde password hashlenmeli
     @Override
     public CreationResult register(RegistrationServiceRequest request) {
         Optional<User> userOptional= repository.findByEmail(request.getEmail());
@@ -36,7 +50,9 @@ public class DefaultRegistrationService implements RegistrationService{
         }
         user.setEmail(request.getEmail());
         user.setDateOfRegistration(LocalDate.now());
-        user.setPassword(request.getPassword());
+        PasswordEncoderAdaptor passwordEncoderAdaptor= new PasswordEncoderAdaptor();
+        String hashedPassword = passwordEncoderAdaptor.encode(request.getPassword());
+        user.setPassword(hashedPassword);
         if(request.getUserType()== UserType.VOLUNTEER){
             user.setUserType(UserType.VOLUNTEER);
         }else{
@@ -44,6 +60,18 @@ public class DefaultRegistrationService implements RegistrationService{
         }
 
         repository.save(user);
+
+        String token= tokenService.generateVerificationToken();
+        VerificationToken verificationToken= new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        Calendar calendar= Calendar.getInstance();
+        calendar.add(Calendar.HOUR, 24);
+        verificationToken.setExpiryDate(calendar.getTime());
+        verificationTokenRepository.save(verificationToken);
+        emailService.sendVerificationEmail(user.getEmail(),token);
+
         return CreationResult.success();
     }
 
