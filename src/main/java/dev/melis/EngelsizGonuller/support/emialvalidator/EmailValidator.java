3a35fef6322b4003014af2de3ba6cb44edd2b969
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:54:15 +0300

    update email validator

--- src/main/java/dev/melis/EngelsizGonuller/support/emialvalidator/EmailValidator.java
+++ src/main/java/dev/melis/EngelsizGonuller/support/emialvalidator/EmailValidator.java
@@ -5,7 +5,8 @@ import java.util.regex.Pattern;
 public class EmailValidator {
 
     private static final String EMAIL_PATTERN =
-        "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
+            + "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
 
     private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
