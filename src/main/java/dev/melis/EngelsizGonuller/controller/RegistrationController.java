8c0ac07d1f61d2042774bc887aac7313009cb479
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:53:39 +0300

    update RegistrationController

--- src/main/java/dev/melis/EngelsizGonuller/controller/RegistrationController.java
+++ src/main/java/dev/melis/EngelsizGonuller/controller/RegistrationController.java
@@ -1,12 +1,15 @@
 package dev.melis.EngelsizGonuller.controller;
 
+import dev.melis.EngelsizGonuller.service.email.VerificationTokenService;
 import dev.melis.EngelsizGonuller.service.registration.RegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class RegistrationController {
 
     private final RegistrationService registrationService;
+    private final VerificationTokenService verificationTokenService;
 
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, VerificationTokenService verificationTokenService) {
         this.registrationService = registrationService;
+        this.verificationTokenService = verificationTokenService;
     }
 
     @PostMapping("/auth/register")
@@ -26,6 +31,16 @@ public class RegistrationController {
             return new ResponseEntity<>("User has already registered",HttpStatus.CONFLICT);
         }
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<?> verifyEmail(@RequestParam("token") String token){
+        boolean result= verificationTokenService.validateVerificationToken(token);
+        if(result){
+            return new ResponseEntity<>("Verification token validated",HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>("Verification token not valid",HttpStatus.CONFLICT);
     }
 
 }
