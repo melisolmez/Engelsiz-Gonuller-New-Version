3c2dd79a0783fe255d28fedc3245bb7ea5c16bb4
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:52:38 +0300

    update VerificationToken

--- src/main/java/dev/melis/EngelsizGonuller/repository/VerificationTokenRepository.java
+++ src/main/java/dev/melis/EngelsizGonuller/repository/VerificationTokenRepository.java
@@ -1,11 +1,11 @@
 package dev.melis.EngelsizGonuller.repository;
 
 import dev.melis.EngelsizGonuller.model.VerificationToken;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmailRegistrationTokenRepository extends JpaRepository<VerificationToken,Long> {
-
-    VerificationToken findByToken(String token);
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken,Long> {
+    Optional<VerificationToken> findByToken(String token);
 }
