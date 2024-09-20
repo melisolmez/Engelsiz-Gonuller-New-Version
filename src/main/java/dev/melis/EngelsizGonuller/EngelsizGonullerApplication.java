e12e73761c543f389be71ca0046b2c220b192da9
Melis Olmez <olmezmelis204@gmail.com>
2024-09-20 23:56:00 +0300

    update project

--- src/main/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplication.java
+++ src/main/java/dev/melis/EngelsizGonuller/EngelsizGonullerApplication.java
@@ -2,9 +2,12 @@ package dev.melis.EngelsizGonuller;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class EngelsizGonullerApplication {
 
 	public static void main(String[] args) {
